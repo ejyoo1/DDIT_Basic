43b19d9ace2c0fc5fa9073aaebc68915988b5639
ejyoo1 <o3k3585@gmail.com>
2021-01-18 13:51:05 +0900

    modified play baseball game.

--- workspace/JavaBasic/src/c_statement/Baseball.java
+++ workspace/JavaBasic/src/c_statement/Baseball.java
@@ -8,26 +8,39 @@ public class Baseball {
 		 * 135 123 : 1S 1B 1O 531 : 1S 2B 0O 321 : 0S 2B 1O
 		 * 
 		 * 스트라이크 : 1이라는 숫자와 자리수도같을 때 볼 : 3이라는 숫자가 있지만 자리수가 다름 아웃 : 아예 없을 경우
+		 * 
+		 * 본 코드에서 해결되지 않은 문제 : 사용자 입력이 null 일때 해결이 안됨.
 		 */
 		// 1부터 9까지의 숫자를 3개 발생시키고 겹치지 않게
 		int random1 = (int) (Math.random() * 9) + 1;
 		int random2 = (int) (Math.random() * 9) + 1;
 		int random3 = (int) (Math.random() * 9) + 1;
-
+		
+		//몇회 시도인지 판별
+		int count = 1;
 		// 게임종료 flag
 		boolean result = true;
 
+		while (true) {
+			random1 = (int) (Math.random() * 9) + 1;
+			random2 = (int) (Math.random() * 9) + 1;
+			random3 = (int) (Math.random() * 9) + 1;
+			if(random1 != random2 && random1 != random3 && random2 != random3){
+				break;
+			}
+		}
+		
+		/*
+		do{
+			random1 = (int) (Math.random() * 9) + 1;
+			random2 = (int) (Math.random() * 9) + 1;
+			random3 = (int) (Math.random() * 9) + 1;
+		}while(random1 == random2 || random2 == random3 || random2 == random3);
+		*/
+		
 		// 스트라이크가 3개가 될 때 까지 무한반복
 		while (result) {
 			//
-			while (true) {
-				random1 = (int) (Math.random() * 9) + 1;
-				random2 = (int) (Math.random() * 9) + 1;
-				random3 = (int) (Math.random() * 9) + 1;
-				if(random1 != random2 && random1 != random3 && random2 != random3){
-					break;
-				}
-			}
 			int strike = 0;
 			int ball = 0;
 			int out = 0;
@@ -44,7 +57,18 @@ public class Baseball {
 			int userNum2 = Integer.parseInt(sc.nextLine());
 			System.out.print("세번째 숫자 입력>");
 			int userNum3 = Integer.parseInt(sc.nextLine());
-
+			
+			/*
+			 * 3자리수 한번에 받기
+			System.out.print("3자리 숫자>");
+			int input = Integer.parseInt(s.nextLine());
+			int i3 = input % 10;
+			input /= 10;
+			int i2 = input % 10;
+			input /= 10;
+			int i1 = input % 10;
+			*/
+			
 			/*
 			 * // 스트라이크==================
 			 * 
@@ -97,6 +121,8 @@ public class Baseball {
 			}
 
 			// 결과 : =======================
+			System.out.println("==============================");
+			System.out.print(count+"차 시도 결과 : ");
 			System.out.println("스트라이크 : " + strike + " 볼 : " + ball + " 아웃 : "
 					+ out);
 
@@ -105,9 +131,12 @@ public class Baseball {
 				System.out.println("성공!!!!!!!!!야구게임을 종료합니다.");
 				break;
 			} else if (strike == 2) {
-				System.out.println("오오!!! 조금만 더!!!!");
+				
+				System.out.println("오오!!! 조금만 더 시도해 보세요");
+				count++;
 			} else {
 				System.out.println("잘 생각해 보시오ㅋㅋ");
+				count++;
 			}
 			System.out.println("==========================");
 		}
