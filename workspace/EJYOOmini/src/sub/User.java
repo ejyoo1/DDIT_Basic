65ca6f50aceab9b88cb764b9714a325d028f0b5c
ejyoo1 <o3k3585@gmail.com>
2021-01-30 14:33:33 +0900

    Java dying message game Method separation

--- workspace/EJYOOmini/src/sub/User.java
+++ workspace/EJYOOmini/src/sub/User.java
@@ -22,7 +22,8 @@ public class User {
 		if(hintcount==1) {
 			ht = new Hint("범인힌트1-1","범인힌트1-2","살해동기1-1","살해동기1-2","도구1-1","도구1-2");
 			ht.hintPrintln (hintcount);
-			firstReasoning();
+			System.out.println ("첫번째 추리를 시작합니다. 이번 사건과 관련없는 카드 6개를 입력하세요. [카드입력형식] : '카드이름1,카드이름2,...,카드이름5'");
+			reasoningProcess();
 		}else if(hintcount==2) {
 			ht = new Hint("범인힌트2-1","범인힌트2-2","살해동기2-1","살해동기2-2","도구2-1","도구2-2");
 			ht.hintPrintln (hintcount);
@@ -33,11 +34,20 @@ public class User {
 		return hintcount;
 	}
 	
-	void firstReasoning(){//첫번째 추리
+	void reasoningProcess(){//추리 시 try 관련 질문
+		System.out.println ("선택한 6장의 카드를 제거하겠습니까? (YES/NO)");
+		Scanner sc = new Scanner(System.in);
+		String userInput = sc.nextLine ();
+		if(userInput.equals ("YES")) {
+			//검사
+		}else {
+			
+		}
+	}
+	
+	void reasoningInput() {
 		int userAnswerCount = 0;
 		Scanner sc = new Scanner(System.in);
-		System.out.println ("첫번째 추리를 시작합니다. 이번 사건과 관련없는 카드 6개를 입력하세요. [카드입력형식] : '카드이름1,카드이름2,...,카드이름5'");
-		
 		while(true) {
 			System.out.println ("[제거] 범인이 아닐 것 같은 직업이 있습니까? 있다면 입력하세요.");
 			String[] userAnswerCriminal = sc.nextLine ().split (",");
@@ -55,11 +65,14 @@ public class User {
 			}else if(userAnswerCount>6){
 				System.out.println ("제거할 카드를 많이 입력하였습니다. 이번 사건과 관련 없는 카드를 6개 입력해야 합니다.");
 				userAnswerCount = 0;
-			}else {
+			}else {//선택한 카드가 6장인 경우 
+				reasoningProcess();
 				break;
 			}
 		}
 	}
+	
+	
 	
 	void userSuccess() {
 		Scanner sc = new Scanner(System.in);
