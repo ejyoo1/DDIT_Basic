03f35fc5089cdb34931edbc52531a9ead2366735
ejyoo1 <o3k3585@gmail.com>
2021-02-05 18:11:43 +0900

    Java 숫자 3자리수 마다 콤마찍기
    2021-02-05 숫자 3자리 수 마다 콤마찍기 최종완성

--- workspace/JavaBasic/src/i_api/SetComma.java
+++ workspace/JavaBasic/src/i_api/SetComma.java
@@ -7,30 +7,42 @@ public class SetComma {
 	//숫자를 입력받아 입력받은 숫자 3자리 마다 콤마(,)를 붙여 출력해주세요.
 	//1,234,567
 	public static void main (String[] args) {
+//		Scanner sc = new Scanner(System.in);
+//		String str = sc.nextLine();
+//		//문자열 뒤집기
+//		int count = 0;
+//		String rev = "";
+//		for(int i = str.length () - 1 ; 0 <= i ; i--) {
+//			if(count == 3) {
+//				rev += ",";
+//				count = 0;
+//				rev += str.charAt (i);
+//				count++;
+//			}else {
+//				rev += str.charAt (i);
+//				count++;
+//			}
+//		}
+//		System.out.println (rev);
+//		
+//		
+//		for(int i = rev.length()-1 ; i >= 0; i--) {
+//			char charAt = rev.charAt (i);
+//			System.out.print (charAt);
+//		}
 		Scanner sc = new Scanner(System.in);
-		String str = sc.nextLine();
-		//문자열 뒤집기
-		int count = 0;
-		String rev = "";
-		for(int i = str.length () - 1 ; 0 <= i ; i--) {
-			if(count == 3) {
-				rev += ",";
-				count = 0;
-				rev += str.charAt (i);
-				count++;
-			}else {
-				rev += str.charAt (i);
-				count++;
-			}
-		}
-		System.out.println (rev);
-		
-		
-		for(int i = rev.length()-1 ; i >= 0; i--) {
-			char charAt = rev.charAt (i);
-			System.out.print (charAt);
-		}
+		String userValue = sc.nextLine();
+	    String resultValue = "";
+	    for(int i = userValue.length () - 1 ; 0 <= i ; i--) {
+	    	if(i > 0 && (i%3) == 0) {
+	    		System.out.print (userValue.charAt (userValue.length() - i - 1) + "," + resultValue);
+	    	}else {
+	    		System.out.print (userValue.charAt (userValue.length() - i - 1) + resultValue);
+	    	}
+	    		
+	    }
+	    
 	}
-	
+
 }
 
