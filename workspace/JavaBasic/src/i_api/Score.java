93f3d8946007d7b4f7a2f475b6370472111e6b36
ejyoo1 <o3k3585@gmail.com>
2021-02-09 14:04:02 +0900

    2021-02-09 학생 점수,평균관리
    학생 점수 평균관리 코드 정리

--- workspace/JavaBasic/src/i_api/Score.java
+++ workspace/JavaBasic/src/i_api/Score.java
@@ -20,41 +20,35 @@ public class Score {
 		
 //		점수삽입
 		ArrayList<ArrayList<Integer>> scores = new ArrayList<>();
-//		합계 삽입
-		ArrayList<Integer> sums = new ArrayList<>();
-		for(int i = 0 ; i < students.size () ; i++) {
-//			합계변수
-			int sum = 0;
+		for(int i = 0; i < students.size(); i++){
 			ArrayList<Integer> score = new ArrayList<>();
-			for(int j = 0 ; j < subjects.size() ; j++) {
-				score.add ((int)(Math.random () * 101));
-				sum += score.get (j);
-			}			
-			scores.add (score);
-			sums.add (sum);
+			for(int j = 0; j < subjects.size(); j++){
+				score.add((int)(Math.random() * 101));
+			}
+			scores.add(score);
 		}
 		
-//		평균 계산
+//		합계,평균 계산
+		ArrayList<Integer> sums = new ArrayList<>();
 		ArrayList<Double> avgs = new ArrayList<>();
-		for(int i = 0 ; i < sums.size () ; i++) {
-			avgs.add (Math.round(((double)sums.get (i)/ subjects.size())*10)/10.0);
+		for(int i = 0; i < scores.size(); i++){
+			int sum = 0;
+			for(int j = 0; j < scores.get(i).size(); j++){
+				sum += scores.get(i).get(j);
+			}
+			sums.add(sum);
+			avgs.add(Math.round((double)sum / subjects.size() * 100) / 100.0);
 		}
 		
 //		석차 구하기
-		ArrayList<Integer> rank = new ArrayList<>();
-//		석차 1등으로 초기화
-		for(int i = 0 ; i < scores.size (); i++) {
-			rank.add (1);
-		}
-//		석차 계산
-		for(int i = 0 ; i < scores.size () ; i++) {
-			int result = rank.get (i);
-			for(int j = 0 ; j < scores.size () ; j++) {
-				if(sums.get (i) < sums.get (j)) {
-					result++;
+		ArrayList<Integer> ranks = new ArrayList<>();
+		for(int i = 0; i < sums.size(); i++){
+			ranks.add(1);
+			for(int j = 0; j < sums.size(); j++){
+				if(sums.get(i) < sums.get(j)){
+					ranks.set(i, ranks.get(i) + 1);
 				}
 			}
-			rank.set (i,result);
 		}
 		
 		//석차순으로 정렬(시험에서 출제되지 않음)
@@ -65,22 +59,27 @@ public class Score {
 					min = j;
 				}
 			}
+//			점수정렬
 			ArrayList<Integer> temp = scores.get (i);
 			scores.set(i, scores.get (min));
 			scores.set(min, temp);
 			
+//			합계정렬
 			int temp2 = sums.get (i);
 			sums.set(i, sums.get(min));
 			sums.set(min, temp2);
 			
+//			평균정렬
 			double temp3 = avgs.get(i);			
 			avgs.set(i, avgs.get(min));
 			avgs.set(min, temp3);
+
+//			석차정렬
+			int temp4 = ranks.get(i);		
+			ranks.set(i, ranks.get(min));
+			ranks.set(min, temp4);
 			
-			int temp4 = rank.get(i);		
-			rank.set(i, rank.get(min));
-			rank.set(min, temp4);
-			
+//			학생정렬
 			String temp5 = students.get(i);		
 			students.set(i, students.get(min));
 			students.set(min, temp5);
@@ -115,7 +114,7 @@ public class Score {
 				System.out.print(scores.get (i).get (j) + "\t");
 			}
 //			합계,평균,석차 출력
-			System.out.println(sums.get (i) + "\t" + avgs.get (i) + "\t" + rank.get (i));
+			System.out.println(sums.get (i) + "\t" + avgs.get (i) + "\t" + ranks.get (i));
 		}
 		
 		System.out.print("과목합계\t");
