e4c270f82545a8ff8775cb9688990dfb971a9219
ejyoo1 <o3k3585@gmail.com>
2021-02-10 17:19:35 +0900

    2021-02-10 Java-DB연동 초급 프로젝트 인터페이스 생성
    Java-DB연동 초급프로젝트 인터페이스 생성(로그인,이슈생성,이슈조회)

---
+++ workspace/IssueManagement/src/IssueManagement.java
@@ -0,0 +1,9 @@
+
+public interface IssueManagement {
+	public abstract void login();
+	public abstract void issueReadAll();
+	public abstract void issueCreate();
+	public abstract void issueRead();
+	public abstract void issueUpdate();
+	public abstract void issueDelete();
+}
