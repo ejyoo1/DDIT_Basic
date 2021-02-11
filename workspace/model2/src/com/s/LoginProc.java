c051029654e010fa4f6c82ebca1fd124c46bc261
ejyoo1 <o3k3585@gmail.com>
2021-02-11 10:36:26 +0900

    2021-02-11 커넥션 풀로부터 객체 획득

--- workspace/model2/src/com/s/LoginProc.java
+++ workspace/model2/src/com/s/LoginProc.java
@@ -1,15 +1,21 @@
 package com.s;
 
 import java.io.IOException;
+import java.sql.Connection;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 
 @WebServlet(name = "loginProc", description = "로그인 처리", urlPatterns = { "/loginProc" })
 public class LoginProc extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	Connection conn;
        
     public LoginProc() {
         super();
@@ -18,7 +24,22 @@ public class LoginProc extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		try {
+			Context initCtx = new InitialContext();
+			Context envCtx 	= (Context)initCtx.lookup ("java:comp/env");
+			DataSource ds 	= (DataSource)envCtx.lookup("jdbc/java");
+			conn 			= ds.getConnection ();
+			System.out.println ("커넥션 풀로부터 커넥션 객체 획득");
+			
+			//쿼리 수행
+			
+			//반납
+			conn.close ();
+			System.out.println ("커넥션풀로부터 커넥션 객체 반납");
+		} catch (Exception e) {
+			System.out.println ("커넥션 객체 획득 오류" + e.getMessage ());
+		}
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
