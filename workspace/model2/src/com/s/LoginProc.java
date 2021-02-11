a8ff9b9bf133127526a2b21d8ebc2f2d4a03c601
ejyoo1 <o3k3585@gmail.com>
2021-02-11 10:31:51 +0900

    2021-02-11 MVC 모델 2 초기 세팅 (pom.xml, context.xml, web.xml)

---
+++ workspace/model2/src/com/s/LoginProc.java
@@ -0,0 +1,29 @@
+package com.s;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "loginProc", description = "로그인 처리", urlPatterns = { "/loginProc" })
+public class LoginProc extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public LoginProc() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
