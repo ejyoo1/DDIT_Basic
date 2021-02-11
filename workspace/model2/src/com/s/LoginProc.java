19f6729bd2286ec6447b3d4f29e68593f0abf3a4
ejyoo1 <o3k3585@gmail.com>
2021-02-11 13:04:34 +0900

    2021-02-11 MVC2 모델 적용. 커넥션 풀 객체 획득 및 로그인 처리 구현

--- workspace/model2/src/com/s/LoginProc.java
+++ workspace/model2/src/com/s/LoginProc.java
@@ -2,9 +2,12 @@ package com.s;
 
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,6 +15,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
 
+import model1.User;
+//Controller
 @WebServlet(name = "loginProc", description = "로그인 처리", urlPatterns = { "/loginProc" })
 public class LoginProc extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -33,10 +38,43 @@ public class LoginProc extends HttpServlet {
 			System.out.println ("커넥션 풀로부터 커넥션 객체 획득");
 			
 			//쿼리 수행
+			String uid		= request.getParameter ("uid");//"guest"
+			String upw		= request.getParameter ("upw");//"1234"
+//			String sql = "select * from tbl_user where userid='" + uid + "' and userpw='" + upw + "'";
+			String sql		= "select * from tbl_user where userid=? and userpw=?";
+			PreparedStatement ps = conn.prepareStatement (sql);
+			ps.setString(1, uid);
+			ps.setString (2, upw);
+			ResultSet rs = ps.executeQuery ();
+
 			
+//			model
+			User userBean = null;
+			if(rs.next()) {
+				userBean = new User ();
+				userBean.setIdx (rs.getInt ("idx"));
+				userBean.setUid (rs.getString ("userid"));
+				userBean.setUpw (rs.getString ("userpw"));
+				userBean.setRegdate (rs.getString ("regdate"));
+			}
+						
 			//반납
+			rs.close ();
+			ps.close (); 
+//			stmt.close ();
 			conn.close ();
 			System.out.println ("커넥션풀로부터 커넥션 객체 반납");
+			
+			//디스페쳐로 전달
+			if(userBean == null) {
+				RequestDispatcher rd = request.getRequestDispatcher ("loginError.jsp");
+			}else {
+				request.setAttribute ("user", userBean);
+//				bbs.jsp : View
+				RequestDispatcher rd = request.getRequestDispatcher ("bbs2.jsp");
+				rd.forward (request, response);
+			}
+			
 		} catch (Exception e) {
 			System.out.println ("커넥션 객체 획득 오류" + e.getMessage ());
 		}
