7471e5023d460f42534dd3abd4e13f6382cade03
ejyoo1 <o3k3585@gmail.com>
2021-02-16 18:43:13 +0900

    2021-02-16 한줄 출력 쿼리용 메서드
    1)Map<String,Object> selectOne() 메서드 구현

--- workspace/JavaBasic/src/k_jdbc/JDBCUtil.java
+++ workspace/JavaBasic/src/k_jdbc/JDBCUtil.java
@@ -59,7 +59,80 @@ public class JDBCUtil {
 	 * int update(String sql, List<Object> param){}
 	 * 
 	 * */
+//	한줄 쿼리
+	Map<String, Object> selectOne(String sql){
+		Map<String, Object> row = new HashMap<>();
+		try {
+//			DB연결
+			con = DriverManager.getConnection (url,id,pw);
+//			쿼리실행
+			ps = con.prepareStatement (sql);
+//			실행(select)
+			rs = ps.executeQuery ();
+//			resultSet에 무슨 데이터가 잇는지 모르기에 메타데이터를 얻고
+			ResultSetMetaData metaData = rs.getMetaData ();
+//			컬럼 수를 알면 데이터를 뽑을 수 잇음.
+			int columnCount = metaData.getColumnCount ();
+//			값을 추출
+			if(rs.next()) {//true
+				for(int i = 1 ; i <= columnCount ; i++) {
+	//				추출한 내용을 HashMap<String, Object> 에 담아서 리턴함.
+	//				해쉬맵에 put 해서 저장(키:컬럼명, 값:컬럼값)
+	//				무엇을 가져왓는지 모르기에 getObject로 받음
+					row.put (metaData.getColumnName (i), rs.getObject (i));
+				}//close for
+			}//close if
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+//			객체 반환
+			if( rs != null ) try { rs.close (); } catch(Exception e) {}
+			if( ps != null ) try { ps.close (); } catch(Exception e) {}
+			if( con != null ) try { con.close (); } catch(Exception e) {}
+		}//close finally
+		return row;		
+	}
 	
+//	물음표가 없는 select
+	List<Map<String, Object>> selectList(String sql){
+		List<Map<String, Object>> list = new ArrayList<>();
+		try {
+//			DB 연결
+			con = DriverManager.getConnection (url,id,pw);
+//			쿼리 실행
+			ps = con.prepareStatement (sql);
+//			실행(select)
+			rs = ps.executeQuery ();
+//			resultSet에 무슨 데이터가 있는지 모르기에 메타데이터를 얻고
+			ResultSetMetaData metaData = rs.getMetaData ();
+//			컬럼 수를 알면 데이터를 뽑을 수 잇음.
+			int columnCount = metaData.getColumnCount ();
+//			값을 추출
+			while(rs.next()) {
+//				한줄한줄 가져온 행을 해쉬맵에 담아서(우리가 알고있는 맵이 해쉬맵뿐) list에 저장해야 하는 목적을 가진 hashmap을 생성함.
+				HashMap<String, Object> row = new HashMap<>();
+//				한줄 한줄을 해쉬맵에 담음.
+				for(int i = 1 ; i <= columnCount ; i++) {
+//					추출한 내용을 List<Map<String, Object>> list = new ArrayList<>();에 담아서 리턴하는 목적임.
+//					한줄을 해쉬맵 형태로 만들어서 담아야함. 그래서 for문 돌리기 전에 hashmap을 만듬
+//					해쉬맵에 put 해서 저장(키:컬럼명, 값:컬럼값)
+//					무엇을 가져왔을 지 모르기에 getObject로 받음.
+					row.put (metaData.getColumnName (i), rs.getObject (i));
+				}//close for
+//				arrList에 저장
+				list.add (row);
+			}//close while
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+//			객체 반환
+			if( rs != null ) try { rs.close (); } catch(Exception e) {}
+			if( ps != null ) try { ps.close (); } catch(Exception e) {}
+			if( con != null ) try { con.close (); } catch(Exception e) {}
+		}//close finally
+		
+		return list;
+	}
 //	해야될것? 
 //	DB 연결 -> 넘어온 쿼리 실행 -> 물음표가 있으니 list param에 있는것을 sql에 삽입 -> result 셋이 왓으면 값을 추출한 뒤 -> list<map>형식으로 만들어서 리턴을 함.
 	List<Map<String, Object>> selectList(String sql, List<Object> param){
