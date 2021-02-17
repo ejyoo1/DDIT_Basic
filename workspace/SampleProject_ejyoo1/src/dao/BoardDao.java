11dedd6e65593ad88350ca815d7bda815689dba9
ejyoo1 <o3k3585@gmail.com>
2021-02-17 18:42:04 +0900

    작업완료.
    1) 게시글 수정 구현 완료
    2) 게시글 삭제 구현 완료
    3) 로그아웃 구현 완료

--- workspace/SampleProject_ejyoo1/src/dao/BoardDao.java
+++ workspace/SampleProject_ejyoo1/src/dao/BoardDao.java
@@ -1,8 +1,10 @@
 package dao;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import controller.Controller;
 import util.JDBCUtil;
 
 public class BoardDao {
@@ -27,12 +29,67 @@ public class BoardDao {
 //	42. 게시판의 목록을 조회하는 메서드 생성
 	public List<Map<String, Object>> selectBoardList(){
 //		43. 쿼리 작성
-		String sql = "select a.board_no, a.title, a.content, a.user_id, a.user_name, a.reg_date"
-				+ " from tb_jdbc_board a"
-				+ " left outer join tb_jdbc_user b"
-				+ " on a.user_id = b.user_id"
-				+ " order by a.board_no desc";
+		String sql = " SELECT A.BOARD_NO, A.TITLE, A.CONTENT, A.USER_ID, A.USER_NAME, A.REG_DATE"
+				+ " FROM TB_JDBC_BOARD A"
+				+ " LEFT OUTER JOIN TB_JDBC_USER B"
+				+ " ON A.USER_ID = B.USER_ID"
+				+ " ORDER BY A.BOARD_NO DESC";
 //		44. 조회한 결과를 리스트에 담아서 서비스에 보내준다.
 		return jdbc.selectList(sql);
 	}
+//	51. 게시글 등록 메서드
+	public int insertBoard (Map<String, Object> param) {
+//		52. JDBCUtil에 넘겨줄 쿼리를 작성한다.
+		String sql = " INSERT INTO TB_JDBC_BOARD(BOARD_NO,TITLE,CONTENT,USER_ID,REG_DATE,USER_NAME)\r\n"
+				+ "VALUES(BOARD_NO_SEQ.NEXTVAL,?,?,?,SYSDATE,?)";
+//		53. 쿼리에 물음표가 있으므로 물음표 값을 담을 ArrayList를 생성함.
+		List<Object> p = new ArrayList<>();
+		p.add (param.get ("TITLE"));
+		p.add (param.get ("CONTENT"));
+		p.add (Controller.loginUser.get ("USER_ID"));
+		p.add (Controller.loginUser.get ("USER_NAME"));
+//		54. jdbc 접근하여 데이터 insert 후 결과 값을 boardService로 리턴
+		return jdbc.update (sql, p);
+	}
+//	63. 한개의 게시글을 조회
+	public Map<String, Object> selectBoard (Map<String, Object> param) {
+//		64. JDBCUtil에 넘겨줄 쿼리를 작성한다.
+		String sql = " select * from tb_jdbc_board where board_no=?";
+//		65. 쿼리에 물음표가 잇으므로 물음표 값을 담을 ArrayList를 생성한다.
+		List<Object> p = new ArrayList<>();
+		p.add (param.get ("BOARD_NO"));
+//		66. jdbc 접근하여 데이터 조회 후 결과값을 boardService로 리턴
+		return jdbc.selectOne(sql, p);
+	}
+//	75. 게시글 업데이트
+	public int updateBoard (Map<String, Object> param, int input) {
+//		76. 사용자가 수정한 행위 별로 sql과 list 처리
+		String sql = "";
+		List<Object> p = new ArrayList<>();
+		if(input==1) {
+			sql = " UPDATE TB_JDBC_BOARD SET TITLE = ? WHERE BOARD_NO = ?";
+			p.add (param.get("TITLE"));
+		}else if(input==2) {
+			sql = " UPDATE TB_JDBC_BOARD SET CONTENT = ? WHERE BOARD_NO = ?";
+			p.add (param.get ("CONTENT"));
+		}else if(input==3) {
+			sql = " UPDATE TB_JDBC_BOARD SET TITLE = ?,CONTENT = ? WHERE BOARD_NO = ?";
+			p.add (param.get("TITLE"));
+			p.add (param.get ("CONTENT"));
+		}
+		p.add (param.get ("BOARD_NO"));
+//		77. jdbc 접근하여 데이터 update 후 결과 값을 boardService로 리턴
+		return jdbc.update (sql, p);
+	}
+//	85. 게시글 삭제
+	public int deleteBoard (Map<String, Object> param) {
+//		86.JDBCUtil에 넘겨줄 쿼리를 작성한다.
+		String sql = " DELETE FROM TB_JDBC_BOARD WHERE BOARD_NO = ?";
+//		87. 쿼리에 물음표가 있으므로 물음표 값을 담을 ArrayList를 생성한다.
+		List<Object> p = new ArrayList<>();
+		p.add (param.get ("BOARD_NO"));
+//		88. jdbc 접근하여 데이터 조회 후 결과값을 boardService로 리턴
+		return jdbc.update (sql, p);
+	}
+	
 }
--- workspace/SampleProject_ejyoo1/src/service/BoardService.java
+++ workspace/SampleProject_ejyoo1/src/service/BoardService.java
@@ -1,8 +1,10 @@
 package service;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import controller.Controller;
 import dao.BoardDao;
 import util.ScanUtil;
 import util.View;
@@ -36,7 +38,7 @@ public class BoardService {
 		List<Map<String, Object>> boardList = boardDao.selectBoardList();
 //		출력한다.
 		System.out.println("=======================================");
-		System.out.println("번호\t제목\t작성자\t작성일");
+		System.out.println("번호\t제목\t아이디\t이름\t작성일");
 		System.out.println("---------------------------------------");
 		for(Map<String, Object> board : boardList){
 			System.out.print (board.get("BOARD_NO") + "\t");
@@ -50,15 +52,152 @@ public class BoardService {
 		System.out.println("=======================================");
 		System.out.println("1.조회\t2.등록\t0.로그아웃");
 		System.out.print("입력>");
-		
 		int input = ScanUtil.nextInt();
+		
 		switch (input) {
-		case 1:
-			
-			break;
+//		58. 게시글 조회 메서드 호출
+		case 1:	
+			readBoard(); 
+			if(Controller.loginUser==null) {
+				return View.HOME;
+			}else {
+				break;
+			}
+//		47. 게시글 등록 메서드 호출
+		case 2: createBoard(); break;
+//		92. 로그아웃
+		case 0: userLogout(); break;
 		}
 		
 //		목록으로 돌아오도록
+		return View.BOARD_LIST;
+	}
+	public void userLogout () {
+		System.out.println (Controller.loginUser.get ("USER_ID")+"님 로그아웃 되었습니다.");
+		Controller.loginUser = null;
+		System.out.println("=========== 사용자 로그아웃 완료 =============");
+	}
+	//	59. 게시글 조회 메서드
+	public int readBoard () {
+		System.out.println("=========== 게시글 조회 =============");
+//		60. 사용자에게 조회할 게시글 번호를 입력받음
+		System.out.print ("조회할 게시글 번호 입력>");
+		int board_no = ScanUtil.nextInt ();
+		
+//		61. 게시글 번호를 해쉬맵에 데이터를 담음.
+		Map<String, Object> param = new HashMap<>();
+		param.put ("BOARD_NO", board_no);
+		
+//		62. DB에 접근할 BoardDao에 게시글 번호를 파라미터로 담아 호출한다.
+//		67. BoardDao에서 게시글 번호에 해당하는 게시글을 가져와 저장한다.
+		Map<String, Object> board = boardDao.selectBoard(param);
+//		68. 게시글 내용 출력
+		System.out.println ("번호 : " + board.get ("BOARD_NO"));
+		System.out.println ("제목 : " + board.get ("TITLE"));
+		System.out.println ("내용 : " + board.get ("CONTENT"));
+		System.out.println ("아이디 : " + board.get ("USER_ID"));
+		System.out.println ("이름 : " + board.get ("USER_NAME"));
+		System.out.println ("작성시간 : " + board.get ("REG_DATE"));
+//		69. 이 게시글을 사용하여 사용자가 어떤 행위를 할지 결정한다.
+		System.out.println("=======================================");
+		System.out.println("1.수정\t2.삭제\t0.로그아웃");
+		System.out.print("입력>");
+		int input = ScanUtil.nextInt();
+		
+		switch (input) {
+//			70. 수정할 게시글 번호를 담은 수정 메서드 호출
+			case 1: updateBoard(board_no); break;
+//			81. 게시글 삭제할 번호를 담은 삭제 메서드 호출
+			case 2: deleteBoard(board_no); break;
+			case 0: userLogout(); return View.HOME;
+		}
+		
+//		조회나 삭제가 끝나면 목록으로 돌아오도록
+		return View.BOARD_LIST;
+	}
+//	82. 게시글 삭제 메서드
+	public int deleteBoard (int board_no) {
+		System.out.println("=========== 게시글 삭제 =============");
+//		83. 게시글 번호를 HashMap에 담음
+		Map<String, Object> param = new HashMap<>();
+		param.put ("BOARD_NO", board_no);
+//		84. 데이터베이스를 수행할 boardDao에 게시글 업데이트 메서드 호출
+//		89. delete 문을 수행한 결과를 int 변수에 저장한다.
+		int result = boardDao.deleteBoard(param);
+//		90. 결과 유효성 검사.
+		if(0 < result) {
+			System.out.println ("게시글 삭제 성공");
+		}else {
+			System.out.println ("게시글 삭제 실패");
+		}
+//		91. 게시글 삭제 성공 실패 시 게시글 목록을 출력함.
+		return View.BOARD_LIST;
+	}
+	//	71. 게시글 수정 메서드
+	public int updateBoard (int board_no) {
+		System.out.println("=========== 게시글 수정 =============");
+		System.out.println("1.제목수정\t2.내용수정\t3.제목+내용수정");
+		System.out.print("입력>");
+		int input = ScanUtil.nextInt();
+//		72. 게시글 번호, 제목, 내용을HashMap에 담음
+		Map<String, Object> param = new HashMap<>();
+		param.put ("BOARD_NO", board_no);
+//		73.수정할 행위 선택한 후 행위에 따라 map에 담음
+		switch(input) {
+			case 1:
+				System.out.print("게시글 제목>");
+				String title = ScanUtil.nextLine ();
+				param.put ("TITLE", title);	
+				break;
+			case 2: 
+				System.out.print("게시글 내용>");
+				String content = ScanUtil.nextLine ();
+				param.put ("CONTENT", content);	
+				break;
+			case 3:
+				System.out.print ("게시글 제목>");
+				title = ScanUtil.nextLine ();
+				System.out.print ("게시글 내용>");
+				content = ScanUtil.nextLine ();
+				param.put ("TITLE", title);	
+				param.put ("CONTENT", content);	
+		}
+//		74. 데이터베이스를 수행할 boardDao에 게시글 업데이트 메서드 호출
+//		78. 업데이트 문을 수행한 결과를 int 변수에 저장한다.
+		int result = boardDao.updateBoard(param,input);
+		
+//		79. 결과 유효성 검사.
+		if(0 < result) {
+			System.out.println ("게시글 수정 성공");
+		}else {
+			System.out.println ("게시글 수정 실패");
+		}
+//		80. 게시글 수정 성공 실패 시 게시글 목록을 출력함.
+		return View.BOARD_LIST;
+	}
+	//	48. 게시글 등록 메서드
+	public int createBoard() {
+		System.out.println("=========== 게시글 등록 =============");
+		System.out.print ("게시글 제목 >");
+		String title = ScanUtil.nextLine ();
+		System.out.print ("게시글 내용 >");
+		String content = ScanUtil.nextLine ();
+//		49. 해쉬맵에 입력한 데이터를 담음.
+		Map<String, Object> param = new HashMap<>();
+		param.put ("TITLE", title);
+		param.put ("CONTENT", content);
+//		50. 데이터 베이스를 수행할 boardDao 에 게시글 등록메서드 호출
+//		55. 데이터베이스에 게시글 등록을 완료한 결과값을 가져옴
+		int result = boardDao.insertBoard(param);
+		
+//		56.결과 유효성 검사.
+		if(0 < result) {
+			System.out.println ("게시글 등록 성공");
+		}else {
+			System.out.println ("게시글 등록 실패");
+		}
+		
+//		57. 게시글 생성 완료 또는 실패 시 게시글 목록을 출력함.
 		return View.BOARD_LIST;
 	}
 }
